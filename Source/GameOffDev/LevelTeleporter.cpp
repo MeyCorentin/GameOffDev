fdad0118927ecdbfc4fb1c4bed16a09d7c7c9343
Corentin Mey <91592782+MeyCorentin@users.noreply.github.com>
2024-11-19 15:03:54 +0900

    Level Teleporter by Mesh

---
+++ Source/GameOffDev/LevelTeleporter.cpp
@@ -0,0 +1,61 @@
+#include "LevelTeleporter.h"
+#include "Components/StaticMeshComponent.h"
+#include "Engine/StaticMesh.h"
+#include "Engine/World.h"
+#include "Kismet/GameplayStatics.h"
+
+ALevelTeleporter::ALevelTeleporter()
+{
+    PrimaryActorTick.bCanEverTick = true;
+    MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+    RootComponent = MeshComponent;
+    MeshComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+    MeshComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+}
+
+void ALevelTeleporter::BeginPlay()
+{
+    Super::BeginPlay();
+    MeshComponent->OnComponentBeginOverlap.AddDynamic(this, &ALevelTeleporter::LoadLevel);
+}
+
+void ALevelTeleporter::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+bool ALevelTeleporter::CheckCollisionWithPlayer()
+{
+    if (!MeshComponent)
+    {
+        return false;
+    }
+
+    // Récupère le joueur
+    AActor* PlayerActor = GetWorld()->GetFirstPlayerController()->GetPawn();
+    if (PlayerActor)
+    {
+        // Vérifie si le joueur est dans la zone de collision
+        return MeshComponent->IsOverlappingActor(PlayerActor);
+    }
+    return false;
+}
+
+void ALevelTeleporter::LoadLevel(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+    bool bFromSweep, const FHitResult& SweepResult)
+{
+    if (!OtherActor || OtherActor != GetWorld()->GetFirstPlayerController()->GetPawn())
+    {
+        return; // Ignorer si ce n'est pas le joueur
+    }
+
+    if (LevelToLoad.IsNone())
+    {
+        UE_LOG(LogTemp, Warning, TEXT("LevelToLoad n'est pas défini !"));
+        return;
+    }
+
+    // Charge le niveau spécifié
+    UGameplayStatics::OpenLevel(this, LevelToLoad);
+}
---
+++ Source/GameOffDev/LevelTeleporter.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LevelTeleporter.generated.h"
+
+UCLASS()
+class GAMEOFFDEV_API ALevelTeleporter : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ALevelTeleporter();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    virtual void Tick(float DeltaTime) override;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UStaticMeshComponent* MeshComponent;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Level")
+    FName LevelToLoad;
+
+    UFUNCTION(BlueprintCallable, Category = "Level")
+    bool CheckCollisionWithPlayer();
+
+private:
+    UFUNCTION()
+    void LoadLevel(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+        bool bFromSweep, const FHitResult& SweepResult);
+};
